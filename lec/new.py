d31388e592cc0059c70af7cdff2e32be8ca13560
Elza1512 <109744471+Elza1512@users.noreply.github.com>
2022-09-29 21:06:46 +0400

    dz

---
+++ lec/new.py
@@ -0,0 +1,19 @@
+
+value = None
+a = 123
+b = 1.23
+#print(type(a))
+#print(type(b))
+#value = 12334
+#print(type(value))
+s = 'hello'
+print(s) # вывод строки
+print (a, b, s)
+print (a, '-', b, '-', s)
+print ('{1} - {2} - {0}'.format(a, b, s))
+print(f' {a} - {b} - {s}')
+
+f = True
+print(f)
+list = []
+print(list)
